81c017b01132b3132fa4a6088bc35017107dd1ad
pvdo <fire_ra@bk.ru>
2026-03-21 10:09:24 +0300

    debug logs

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"shortener/internal/config"
 )
 
-func main(){
-	cfg:=config.MustLoad()
-	fmt.Println(cfg)
-	//TODO: init logger: log/slog
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log:=setupLogger(cfg.Env)
+	log.Info("starting shortener", "env", cfg.Env)
+	log.Debug("debug messages are enabled")
 	//TODO: init storage sqlite
 	//TODO: init router: chi, "chi render"
 	//TODO: start server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
